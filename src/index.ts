f2c1945888a66e707d1ec90620c2116d2e845a6f
Yash <91452674+xinkin@users.noreply.github.com>
2025-04-15 09:17:23 +0530

    refactor: migrate nft TLD references to ink and add TLD type

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,13 @@
 import { resolveAddress } from './utils/resolveAddress';
 import { resolveDomain } from './utils/resolveDomain';
 import { getMetadata } from './utils/getMetadata';
-import {getTotalPrice} from './utils/getPrice';
+import { getTotalPrice } from './utils/getPrice';
 import { registerDomain } from './utils/registerDomain';
 import type { WalletClient, zeroAddress } from 'viem';
+import { TLD } from '../src/utils/types';
 
 const ZNSConnectClass = class {
-  async resolveAddress(
-    tld:
-      | 'nft'
-      | 'xterio'
-      | 'honey'
-      | 'cz'
-      | 'xlayer'
-      | 'tabi'
-      | 'taiko'
-      | 'scroll',
-    address: `0x${string}`
-  ): Promise<any> {
+  async resolveAddress(tld: TLD, address: `0x${string}`): Promise<any> {
     try {
       return await resolveAddress(tld, address);
     } catch (error) {
@@ -55,45 +45,48 @@ const ZNSConnectClass = class {
     }
   }
 
-async checkDomain(domain: string): Promise<boolean> {
-  try {
-    const domainData:any = await resolveDomain(domain);
-    return domainData.owner !== '0x0000000000000000000000000000000000000000';
-  } catch (error) {
-    console.error('Error checking domain:', error);
-    return false;
+  async checkDomain(domain: string): Promise<boolean> {
+    try {
+      const domainData: any = await resolveDomain(domain);
+      return domainData.owner !== '0x0000000000000000000000000000000000000000';
+    } catch (error) {
+      console.error('Error checking domain:', error);
+      return false;
+    }
   }
-}
 
-async getPrice(domainArray:string[], tld:string):Promise<any>{
-  try{
-    const {totalPrice} = await getTotalPrice(domainArray, tld)
-    return totalPrice
-  }catch(error){
-    console.error('Error getting Price', error);
-    return false;
+  async getPrice(domainArray: string[], tld: string): Promise<any> {
+    try {
+      const { totalPrice } = await getTotalPrice(domainArray, tld);
+      return totalPrice;
+    } catch (error) {
+      console.error('Error getting Price', error);
+      return false;
+    }
   }
-}
 
-async register(  walletClient: WalletClient,
-  domainNames: string[],
-  ownerAddresses: string[],
-  tld: string,
-  referralAddress: string,
-  credits = 0):Promise<any>{
-  try{
-    await registerDomain(walletClient,
-      domainNames,
-      ownerAddresses,
-      tld,
-      referralAddress,
-      credits)
-  }catch(error){
-    console.error('Unable to mint domain(s)', error);
-    throw error;
+  async register(
+    walletClient: WalletClient,
+    domainNames: string[],
+    ownerAddresses: string[],
+    tld: string,
+    referralAddress: string,
+    credits = 0
+  ): Promise<any> {
+    try {
+      await registerDomain(
+        walletClient,
+        domainNames,
+        ownerAddresses,
+        tld,
+        referralAddress,
+        credits
+      );
+    } catch (error) {
+      console.error('Unable to mint domain(s)', error);
+      throw error;
+    }
   }
-}
-
 };
 
 function ZNSConnect() {
